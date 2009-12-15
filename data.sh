239789cb4f566d5d70c05280f4296f01acfa735f
jdcaron <jdcaron@semp3400u.(none)>
2009-12-15 15:46:24 -0500

    Update the backups script so it can't run twice at the same time and also that he does cleanup the files when they are 24 hours old. 	modified:   data.sh

--- data.sh
+++ data.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+if [ -f /tmp/tf2pb ]
+then
+    # Run only one instance of the script on the host.
+    exit
+fi
+touch /tmp/tf2pb
 cd /usr/local/games/steam/orangebox/tf
 for i in `find -maxdepth 1 -name "*\.dem"`
 do
@@ -7,5 +13,8 @@ do
         mv $i ./demos
     fi
 done
-rsync -a --bwlimit=600 ./demos/ tf2pug@tf2pug.org:~/demos.tf2pug.org
-rsync -a --bwlimit=600 ./logs/ tf2pug@tf2pug.org:~/stats.tf2pug.org/logs
+rsync -a --bwlimit=600 --max-size=30000000 ./demos/ tf2pug@tf2pug.org:~/demos.tf2pug.org
+rsync -a --bwlimit=600 --max-size=30000000 ./logs/ tf2pug@tf2pug.org:~/stats.tf2pug.org/logs
+for i in `find ./demos/ -maxdepth 1 -type f -mtime +24`; do rm $i; done
+for i in `find ./logs/ -maxdepth 1 -type f -mtime +24`; do rm $i; done
+rm /tmp/tf2pb
