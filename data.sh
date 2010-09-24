f4f335aab1c1d07dbf2b6465a27886d8a1146239
jdcaron <jdcaron@semp3400u.(none)>
2010-09-24 17:20:49 -0400

    Fix a bug that makes the log files overlap. When they overlap they get overwriten and they are lost. 	modified:   data.sh

--- data.sh
+++ data.sh
@@ -14,18 +14,18 @@ do
         mv $i ./demos
     fi
 done
-for i in `find ./logs -name '*.log' | grep 'L.*[0-9]\.log'`;
+for i in `find ./logs -ctime -1 -name '*.log' | grep 'L.*[0-9]\.log'`;
 do
     # Verify if the file is open by the server proccess and prepend the server name to the logs
     # to guarantee that they have unique files names in the stats page.
     if test -z `fuser $i`
     then
         j=`echo $i | cut -dL -f2`;
-        mv $i ./logs/'dallas3_'$j;
+        cp -u $i ./logs/'chicago2_'$j;
     fi
 done
 rsync -a --bwlimit=600 --max-size=30000000 ./demos/ tf2pug@tf2pug.org:~/demos.tf2pug.org
 rsync -a --bwlimit=600 --max-size=30000000 ./logs/ tf2pug@tf2pug.org:~/stats.tf2pug.org/logs
 for i in `find ./demos/ -maxdepth 1 -type f -mtime +1`; do rm $i; done
-for i in `find ./logs/ -maxdepth 1 -type f -mtime +30`; do rm $i; done
+for i in `find ./logs/ -maxdepth 1 -type f -mtime +1`; do rm $i; done
 rm /tmp/tf2ib
