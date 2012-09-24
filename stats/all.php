99047762d3cf4493b2aa9525ff60e583e951a608
jdcaron <jdcaron@localhost.localdomain>
2012-09-24 12:17:08 -0400

    Adding healing stats. Sending a message to the bot when a new log has been parsed. Adding the team scores. 	modified:   all.php 	modified:   parser.php

--- stats/all.php
+++ stats/all.php
@@ -19,16 +19,16 @@ include 'parser.php';
 
 $database = new Database();
 
-exec('ls -t ~/stats.tf2pug.org/logs/ | grep -v \'L.*[0-9]\.log\' | head -n100', $latestFiles);
+exec('ls -t ~/stats.atf2.org/logs/ | grep -v \'L.*[0-9]\.log\' | head -n100', $latestFiles);
 foreach($latestFiles as $fileName){
     $explodedFileName = explode(".", $fileName);
     $explodedFileName = $database->sqlEscapeString($explodedFileName);
-    if(count($explodedFileName) == 2 && $explodedFileName[1] == log && filesize('./logs/' . $fileName) > 20000){
-        exec('grep \' tv_record \' ~/stats.tf2pug.org/logs/' . $fileName, $mapName);
+    if(count($explodedFileName) == 2 && $explodedFileName[1] == 'log' && filesize('./logs/' . $fileName) > 20000){
+        exec('grep \' tv_record \' ~/stats.atf2.org/logs/' . $fileName, $mapName);
         $mapName = explode(" ", $mapName[0]);
         $mapName = explode("_", $mapName[count($mapName) - 1]);
         $logDate = $mapName[0];
-        $mapName = $mapName[4];
+        $mapName = str_replace('.dem', '', $mapName[4]);
         if($mapName){
             $logDate = str_replace('h', ':', $logDate);
             $logDate = str_replace('m', '', $logDate);
@@ -36,9 +36,12 @@ foreach($latestFiles as $fileName){
             $server = explode("_", $explodedFileName[0]);
             $server = $server[0];
             $newFileName = $server . '_' . $logDate . '_' . $mapName;
+            echo "$newFileName\n";
+            echo "SELECT * FROM files WHERE fileName = '$newFileName'\n";
+            var_dump(!$database->queryAssoc("SELECT * FROM files WHERE fileName = '$newFileName'"));
             if( !$database->queryAssoc("SELECT * FROM files WHERE fileName = '$newFileName'")){
                 $directory = './log/' . $newFileName;
-                $newFileName = $newFileName . '.log';
+                $newFileName = $newFileName . ".log\n";
                 mkdir($directory);
                 copy('./logs/' . $fileName, $directory . '/' . $newFileName);
                 if(!parse($directory, 'ready')){
