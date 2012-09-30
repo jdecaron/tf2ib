3493414d998b0107169c7f79416ff5c59365dfd4
Jean-Denis Caron <jdecaron@gmail.com>
2012-09-30 01:47:23 -0400

    Listening to the statistics message from the statistics system. 	modified:   srcdslistener.py

--- srcdslistener.py
+++ srcdslistener.py
@@ -11,7 +11,7 @@ cursor = database.cursor()
 listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 listener.bind(('173.255.206.107', 27069))
 listener.listen(1)
-servers = ["208.115.210.90", "208.115.210.91", "208.115.210.92", "208.115.210.93", "208.115.210.94"]
+servers = ["173.236.129.73", "208.115.210.90", "208.115.210.91", "208.115.210.92", "208.115.210.93", "208.115.210.94"]
 while 1:
     try:
         connection, address = listener.accept()
@@ -26,6 +26,11 @@ while 1:
     print address[0] 
     if data and address[0] in servers:
         print 'authorized'
-        cursor.execute('INSERT INTO srcds VALUES (%s, %s)', (data, int(time.time())))
-        database.commit()
+        if address[0] == "173.236.129.73":
+            message = "PRIVMSG " + config.channel + " :\x038,01Statistics have been updated: \x0311,01" + data
+            cursor.execute('INSERT INTO messages (message) VALUES (%s)', (message,))
+            cursor.execute('COMMIT;')
+        else:
+            cursor.execute('INSERT INTO srcds VALUES (%s, %s)', (data, int(time.time())))
+            database.commit()
         connection.close()
